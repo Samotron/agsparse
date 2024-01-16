b86a8f6613cc394e4f722818769f340bf79db63c
Sam <james.s.rogers23@gmail.com>
2024-01-16 18:31:24 +0000

    switched allocator version and making test work

--- src/ags.zig
+++ src/ags.zig
@@ -4,10 +4,14 @@ const std = @import("std");
 /// TODO add function to dump to csv
 /// TODO add function to check data rows against the types given
 const ags_table = struct {
-    group: []u8,
-    headings: [][]u8,
-    types: [][]u8,
-    data: [][][]u8,
+    group: []const u8,
+    headings: []const u8,
+    types: []const u8,
+    data: [][]const u8,
+
+    pub fn print_self(self: *const ags_table) !void {
+        std.debug.print("{s}\n", .{self.group});
+    }
 };
 
 /// private enum to store the type of a row for easier if statements
@@ -50,10 +54,7 @@ fn row_processor(row: []const u8) !row_type {
 
 /// this function takes a link to an AGS File and parses
 /// it to a standard struct enabling it to be transformed.
-pub fn parse_ags(link: []const u8) !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer std.debug.assert(gpa.deinit() == .ok);
-    const gpa_ = gpa.allocator();
+pub fn parse_ags(link: []const u8, gpa: std.mem.Allocator) ![]ags_table {
     var temp_buf: [1024]u8 = undefined;
     const cwd = try std.os.getcwd(&temp_buf);
     std.debug.print("{s}\n\n", .{cwd});
@@ -64,22 +65,36 @@ pub fn parse_ags(link: []const u8) !void {
     var in_stream = buf_reader.reader();
 
     var buf: [1024]u8 = undefined;
-    var output = std.ArrayList(ags_table).init(gpa_);
+    var output = std.ArrayList(ags_table).init(gpa);
+    var data = std.ArrayList([]const u8).init(gpa);
     var temp: ags_table = undefined;
 
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         const typer = try row_processor(line);
-        std.debug.print("{s}\n", .{@tagName(typer)});
+        //std.debug.print("{s}\n", .{@tagName(typer)});
         if (typer == row_type.group) {
+            std.debug.print("{s}", .{line});
             temp.group = line;
+        } else if (typer == row_type.types) {
+            temp.types = line;
+        } else if (typer == row_type.heading) {
+            temp.headings = line;
+        } else if (typer == row_type.data) {
+            try data.append(line);
         } else if (typer == row_type.empty) {
             try output.append(temp);
+            temp.data = try data.toOwnedSlice();
             temp = undefined;
+            data = std.ArrayList([]const u8).init(gpa);
         }
     }
+    return try output.toOwnedSlice();
 }
 
 test "test reading a ags file" {
     const link = "/home/samotron/Projects/agsparse/resources/test-ags.ags";
-    try parse_ags(link);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa_ = gpa.allocator();
+    var a = try parse_ags(link, gpa_);
+    std.debug.print("{any}", .{a});
 }
